be23e172ec45844b3d8e2de435fa71ce564e77a3
Miguel Ugalde <migugalde@berkeley.edu>
2017-09-13 21:16:22 -0700

    Completed: hw1 completed and passes autograder tests

--- foobar.rb
+++ foobar.rb
@@ -3,6 +3,15 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+    a.collect! do |item|
+    	item = item.to_i
+    	if item.even? && item < 8
+    		item += 2
+    	else
+    		item = 0
+    	end
+    end
+    a.uniq.sum
   end
 end
 
